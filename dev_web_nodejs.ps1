eab28c46e80ce13a0cdb04565c3c5156e9d2278a
Josh King <joshgking@users.noreply.github.com>
2020-03-10 23:44:36 -0700

    Node version manager and hyperv/docker

--- dev_web_nodejs.ps1
+++ dev_web_nodejs.ps1
@@ -26,6 +26,8 @@ executeScript "SystemConfiguration.ps1";
 executeScript "FileExplorerSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
 executeScript "CommonDevTools.ps1";
+executeScript "HyperV.ps1";
+executeScript "Docker.ps1";
 executeScript "WSL.ps1";
 executeScript "Browsers.ps1";
 
@@ -34,8 +36,8 @@ code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension msjsdiag.debugger-for-edge
 
 #--- Tools ---
-choco install -y nodejs-lts # Node.js LTS, Recommended for most users
-# choco install -y nodejs # Node.js Current, Latest features
+choco install -y nodist # Node.js natural version manager for Windows
+unblock-file -path "C:\Program Files (x86)\Nodist\bin\nodist.ps1" # Powershell nodist support
 choco install -y visualstudio2017buildtools
 choco install -y visualstudio2017-workload-vctools
 choco install -y python2 # Node.js requires Python 2 to build native modules
